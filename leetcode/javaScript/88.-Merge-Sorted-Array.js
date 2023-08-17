f9dbd20a0240715c50a545a4b38381a3512ad7cb
Rafiul29 <rafiulislam4472@gmail.com>
2023-08-17 13:09:34 +0600

    merge two array

--- leetcode/javaScript/88.-Merge-Sorted-Array.js
+++ leetcode/javaScript/88.-Merge-Sorted-Array.js
@@ -1,9 +1,9 @@
 var merge = function(nums1, m, nums2, n) {
-  const newArr1=nums1.slice(0,m)
-  const newArr2=nums2.slice(0,n);
-  let mergeArr=newArr1.concat(newArr2);
- let sort= mergeArr.sort((a,b)=>a-b)
+  nums1.length = m;
+  nums2.length = n;
+  nums1.push(...nums2)
+ let sort= nums1.sort((a,b)=>a-b)
   return sort
 };
 
-console.log(merge([0],0,[1],1))
+console.log(merge([1,2,3,0,0,0],3,[2,5,6],3))
