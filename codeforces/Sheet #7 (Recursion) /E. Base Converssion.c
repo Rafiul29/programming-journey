bd3a8f83831959e2630f4b3c704b5f57564cacaa
Rafiul29 <rafiulislam4472@gmail.com>
2023-11-04 00:51:11 +0600

    new problem solved

--- codeforces/Sheet #7 (Recursion) /E. Base Converssion.c
+++ codeforces/Sheet #7 (Recursion) /E. Base Converssion.c
@@ -3,21 +3,45 @@
 #include <stdio.h>
 
 void Base_Conversion(int n) {
-  if (n == 0)
+  if (n == 0) {
     return;
+  }
   Base_Conversion(n / 2);
-  printf("%d",n%2);
+  printf("%d", n % 2);
 }
 
 int main() {
   int t;
   scanf("%d", &t);
   while (t--) {
-    long long int n;
-    scanf("%lld", &n);
+    int n;
+    scanf("%d", &n);
     Base_Conversion(n);
     printf("\n");
   }
 
+  return 0;
+}
+
+// error
+#include <stdio.h>
+
+int decimal_binary(int n) {
+  if (n == 0)
+    return 0;
+  else
+    return ((n % 2) + 10 * decimal_binary(n / 2));
+}
+
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    int n;
+    scanf("%d", &n);
+    printf("%d", decimal_binary(n));
+    printf("\n");
+  }
+  
   return 0;
 }
