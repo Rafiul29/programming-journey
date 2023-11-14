2bf6f8118f3c736d01e85a8a93f6301eeffc3def
Rafiul29 <rafiulislam4472@gmail.com>
2023-11-14 12:50:26 +0600

    c++ problme solved

--- Mathlab/lab problem/solution-1/1.m
+++ Mathlab/lab problem/solution-1/1.m
@@ -5,39 +5,56 @@ x1=input('Enter starting value for x1: ');
 y1=input('Enter starting value for y1: ');
 x2=input('Enter ending value for x2: ');
 y2=input('Enter ending value for y2: ');
+
 for i=1:640
 x=i;
 y=ymax/2;
 %fprintf("%d %d\n",x,y);
 plot(x,y);
-hold on;
+grid on ,hold on
 end
+
 for i=1:480
 y=i;
 x=xmax/2;
 %fprintf("%d %d\n",x,y);
 plot(x,y);
-hold on;
+grid on ,hold on
 end
-x=x1;
-y=y1;
+
+%
 dx=x2-x1;
 dy=y2-y1;
-%fprintf("%d %d\n",x,y);
-%fprintf("%d %d\n",dx,dy);
+
+%find the max value
 if(abs(dx)>abs(dy))
     s=abs(dx);
 else
-s=abs(dy);
+    s=abs(dy);
 end
-%fprintf("%d",s);
-xinc=dx/s;
-yinc=dy/s;
-%fprintf("%d %d",xinc,yinc);
-for k=1:s
-x=x+xinc;
-y=y+yinc;
-fprintf("%d %d",320+x,240+y);
-plot(x,y,"ok");
-hold on;
+
+%increment x-axis and y-axis
+Xin=dx/s;
+Yin=dy/s;
+
+for i=1:s
+    x=x+Xin;
+    y=x+Yin;
+  plot(round(320+x),round(240+y));
 end
+grid on,hold on;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ codeforces/C++ Programming/ Sheet #3 (Arrays) /H. Sorting.cpp
@@ -0,0 +1,31 @@
+// Online C++ compiler to run C++ progra
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+int n;
+cin>>n;
+int arr[n];
+
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+
+for(int i=0;i<n;i++){
+  for(int j=i+1;j<n;j++){
+      if(arr[i]>arr[j]){
+          swap(arr[i],arr[j]);
+      }
+  }
+}
+
+  for (int i = 0; i < n; i++) {
+    if (i == n - 1) {
+      cout << arr[i];
+    } else {
+      cout << arr[i] << " ";
+    }
+  }
+  return 0;
+}
