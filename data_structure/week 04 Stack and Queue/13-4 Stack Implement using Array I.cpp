5962eaadf97e5ab984082c0d9d868710762b7bc0
Rafiul29 <rafiulislam4472@gmail.com>
2023-12-18 00:53:04 +0600

    stack implementation

---
+++ data_structure/week 04 Stack and Queue/13-4 Stack Implement using Array I.cpp
@@ -0,0 +1,45 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class myStack{
+    public:
+       vector<int> v;
+       
+       void push(int val){
+           v.push_back(val);
+       }
+       
+       void pop(){
+           v.pop_back();
+       }
+       
+       int top(){
+           return v.back();
+       }
+       
+       int size(){
+           return v.size();
+       }
+       bool empty(){
+           if(v.size()==0) return true;
+           else return false;
+       }
+       
+};
+
+int main()
+{
+    myStack st;
+    st.push(15);
+    st.push(25);
+    cout<<st.top()<<endl;
+    st.pop();
+   cout<<st.top()<<endl;
+   st.pop();
+   
+   if(st.empty()==false){
+          cout<<st.top()<<endl;
+   }
+    return 0;
+}
