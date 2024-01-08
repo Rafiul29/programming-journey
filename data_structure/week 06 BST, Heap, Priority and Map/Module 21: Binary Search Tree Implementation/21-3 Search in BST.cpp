c7590590689687d888a44ba0a9da14d6d2304b17
Rafiul29 <rafiulislam4472@gmail.com>
2024-01-08 17:27:28 +0600

    bst

---
+++ data_structure/week 06 BST, Heap, Priority and Map/Module 21: Binary Search Tree Implementation/21-3 Search in BST.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+         
+        Node(int val){
+            this->val=val;
+            this->left=NULL;
+            this->right=NULL;
+        }
+};
+
+Node* input_tree(){
+    int val;
+    cin>>val;
+    Node* root;
+    
+    if(val==-1){
+        root==NULL;
+        return 0;
+    } else{
+        root=new Node(val);
+    } 
+    queue<Node*> q;
+    if(root) q.push(root);
+    while(!q.empty()){
+        Node* p=q.front();
+        q.pop();
+        
+        int l,r;
+        cin>>l>>r;
+        
+        Node* left;
+        Node* right;
+        
+        if(l==-1) left=NULL;
+        else left=new Node(l);
+        
+        if(r==-1) right=NULL;
+        else right=new Node(r);
+        
+        p->left=left;
+        p->right=right;
+        
+        if(p->left) q.push(p->left);
+        if(p->right) q.push(p->right);
+        
+    } 
+    
+    return root;
+}
+
+
+
+void print(Node * root){
+    if(root==NULL) return;
+    cout<<root->val<<" ";
+    print(root->left);
+    print(root->right);
+}
+
+bool search(Node * root,int x){
+    if(root==NULL) return false;
+    if(root->val==x) return true;
+    if(x<root->val){
+        bool l =search(root->left,l);
+        return l;
+    }else{
+        return search(root->right,x);
+    }
+}
+int main()
+{
+   Node* root=input_tree();
+    if(search(root,60)){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    } 
+    return 0;
+}
