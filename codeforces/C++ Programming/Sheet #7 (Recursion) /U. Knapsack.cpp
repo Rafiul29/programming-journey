d29b0716e41cf31811a46094fad9c4dd32faa006
Rafiul29 <rafiulislam4472@gmail.com>
2024-02-19 14:00:03 +0600

    0-1 knapsack algorithm

---
+++ codeforces/C++ Programming/Sheet #7 (Recursion) /U. Knapsack.cpp
@@ -0,0 +1,52 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+const int maxN = 22;
+const int maxW = 1005;
+int dp[maxN][maxW];
+
+int kanpsack(int n, int weight[], int value[], int W)
+{
+  if (n < 0 || W == 0)
+  {
+    return 0;
+  }
+  if (dp[n][W] != -1)
+  {
+    return dp[n][W];
+  }
+  if (weight[n] <= W)
+  {
+    int op1 = kanpsack(n - 1, weight, value, W - weight[n]) + value[n];
+    int op2 = kanpsack(n - 1, weight, value, W);
+    return dp[n][W] = max(op1, op2);
+  }
+  else
+  {
+
+    int op2 = kanpsack(n - 1, weight, value, W);
+    return dp[n][W] = op2;
+  }
+}
+int main()
+{
+
+  int n, W;
+  cin >> n >> W;
+  int weight[n], value[n];
+
+  for (int i = 0; i < n; i++)
+  {
+    cin >> weight[i]>>value[i];
+  }
+ 
+  for (int i = 0; i < maxN; i++)
+  {
+    for (int j = 0; j < maxW; j++)
+    {
+      dp[i][j] = -1;
+    }
+  }
+
+  cout << kanpsack(n - 1, weight, value, W) << endl;
+}
