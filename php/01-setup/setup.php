c9eee26cb1b2100164efc52df390a19e8e22f730
rafiul29 <rafiulislam4472@gmail.com>
2024-03-31 12:54:13 +0600

    python  run

---
+++ php/01-setup/setup.php
@@ -0,0 +1,19 @@
+//01 windows install for php
+install for the php binary
+
+// 02 install debian base 
+sudo apt-get install php
+
+// 02 install mac 
+brew install php
+
+//check version php
+php -v
+
+//php install file location
+which php
+
+
+//install vs code extension
+code runner
+ctrl+shipt+p
