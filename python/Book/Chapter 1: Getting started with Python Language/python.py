a584b9fdaac55c98d09f7f0f34b120858a0c83d6
Rafiul29 <rafiulislam4472@gmial.com>
2024-08-08 14:44:13 +0600

    update

--- python/Book/Chapter 1: Getting started with Python Language/python.py
+++ python/Book/Chapter 1: Getting started with Python Language/python.py
@@ -3,6 +3,10 @@
 """
 ***Python is a widely used high-level programming language for general-purpose programming, created by Guido van Rossum and first released in 1991.
 
+
+
+
+
 ***Python features 
 --dynamic type system  
 --automatic memory management  
