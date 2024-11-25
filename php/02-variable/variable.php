ddf725e243e721457468a84f145bfa7ebbe3cbc5
Rafiul29 <rafiulislam4472@gmail.com>
2024-11-25 10:46:31 +0600

    php code added

---
+++ php/02-variable/variable.php
@@ -0,0 +1,9 @@
+<?php
+$name="rafiul islam";
+$age= 23;
+$department='cse';
+echo "I'm about to learn PHP! \n";
+echo "your name is $name and age $age and department of $department \n";
+echo 'name is '.$name." age is ".$age." department of ".$department;
+echo "\n";
+var_dump($name);
